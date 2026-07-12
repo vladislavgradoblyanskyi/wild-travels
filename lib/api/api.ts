e6355ccd8948e734b418bb2a6f8fd6a67d0f8af2
SCBProd <scbproduktion@gmail.com>
2026-07-12 23:07:07 +0200

    1

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
-  withCredentials: true,
-});
+export const nextServer = axios.create();
+
+// export const nextServer = axios.create({
+// baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+// withCredentials: true,
+// });
 
